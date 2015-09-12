a79f8593ba5a4ea248e0fc6fb002c41e3d869bdf
akshat agarwal <akshat.agarwal@splashmath.com>
2015-09-12 21:53:54 +0530

    some comments

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,6 +1,6 @@
-for f in githooks/* 
-do 
-  echo "Processing file $f" 
+for f in githooks/*
+do
+  echo "Processing file $f"
   FILENAME=$(basename $f)
   echo "ln -s -f ../../githooks/$FILENAME .git/hooks/$FILENAME"
   eval "ln -s -f ../../githooks/$FILENAME .git/hooks/$FILENAME"
